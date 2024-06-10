d547612e86497a7560870722e4723f61cbf82d3d
Haifansantos <santoshaifan@gmail.com>
2024-06-10 11:36:09 +0700

    first commit

---
+++ src/data/CoursesSection.jsx
@@ -0,0 +1,9 @@
+export const coursesSection = {
+    content:  `<p className="deskripsi">JANGAN TERLEWATKAN</p>
+                <h2>Online Courses</h2>
+                <p>Bangun karier dalam pengembangan web dengan kursus online kami! Dapatkan keterampilan praktis dalam merancang dan mengembangkan situs web yang menarik dan responsif. Bergabunglah sekarang untuk belajar dari para ahli industri dan mulai membangun proyek-proyek web yang mengesankan!</p>
+
+                <p><a href="" className="tbl-biru">Pelajari Lebih Lanjut</a></p`,
+
+                image: 'https://img.freepik.com/free-vector/online-learning-isometric-concept_1284-17947.jpg?size=626&ext=jpg&ga=GA1.2.1769275626.1605867161'
+}
---
+++ src/data/HomeSection.jsx
@@ -0,0 +1,8 @@
+export const homeSection = {
+  image:
+    "https://img.freepik.com/free-vector/web-development-programmer-engineering-coding-website-augmented-reality-interface-screens-developer-project-engineer-programming-software-application-design-cartoon-illustration_107791-3863.jpg?size=626&ext=jpg&ga=GA1.2.1769275626.1605867161",
+  content: ` <p className="deskripsi">Export Programming Modal #dirumahaja</p>
+                <h2>Wujudkan Impianmu</h2>
+                <p>Kita usahakan gaji 10 digit itu:).</p>
+                <p><a href="" className="tbl-pink">Pelajari Lebih Lanjut</a></p>`,
+};
